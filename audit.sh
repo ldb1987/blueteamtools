2b3e471e8a055c009b8057e8beca3108ca1f7a9e
ldb1987 <ldb1987@outlook.com>
2024-11-08 17:27:00 -0500

    removed monitor

--- audit.sh
+++ audit.sh
@@ -5,16 +5,15 @@ logDir="$HOME/logs"
 mkdir -p "$logDir"
 grep -vE "nologin" /etc/passwd > "$logDir/users"
 
-systemctl list-units --state=failed > "$logDir/failed-services"
 who > "$logDir/logged-in"
 printf "%s\n\n%s\n\n%s\n\n%s\n\n%s\n\n%s\n" "---Available Users---" "$(< $logDir/users)" "---Logged-In Users---" "$(< $logDir/logged-in)" "---Failed Services---" "$(< $logDir/failed-services)"
 
 getactiveservices() {
-    systemctl list-units --state=active | tee
+    systemctl list-units --state=active | sed "s/^  //g" | grep -oE '^[^ ]{1,}' | tee $logDir/active-services
 }
 
 getfailedservices() {
-    systemctl list-units --state=failed | tee
+    systemctl list-units --state=failed | tee $logDir/failed-services
 }
 
 getreverseshells() {
@@ -32,3 +31,15 @@ checkssh() {
     grep "Port:" /etc/ssh/sshd_config
     grep "PAM" /etc/ssh/sshd_config
 }
+
+auditservices() {
+    active_service_entries_log="$logDir/$(date | sed "s/_//g")active-service-entries"
+    echo "$active_service_entries_log"
+    touch "$active_service_entries_log"
+    while read -r LINE; do
+        sudo systemctl status $LINE >> "$active_service_entries_log"
+        sudo systemctl status $LINE
+    done < "$logDir/active-services"
+}
+getactiveservices
+auditservices
--- cpass.sh
+++ cpass.sh
@@ -1,33 +1,39 @@
 #!/bin/sh
-printf -v lowerc "%s" {qwertyuiopasdfghjklzxcvbnm}
+lowerc='qwertyuiopasdfghjklzxcvbnm'
 upperc='QWERTYUIOPASDFGHJKLZXCVBNM'
 numc='1234567890'
-specc='!@#$%^&*()'
+special='!@#$%^&*(),.<>?]=+-_[};:{'
 
 
-users=($(grep -vE "nologin|$(id -un)|root" /etc/passwd | grep -oE "^[^:]{1,}"))
+#users=($(grep -vE "nologin|$($USER)|root" /etc/passwd | grep -oE "^[^:]{1,}"))
+users=['president']
 echo "This script will randomly change the password of all users on this system with login shells.
         Upon completion, you will be prompted to change your own password.
         Continue? [Y/N]"
-get4() {
-    for s in lowerc upperc numc specc; do
-        echo "${!s}" | sed "s/./&\n/g" | grep . | shuf | head -1
-    done | tr -d "\n"
-}
 
-dopass() {
-    pass=''
-    for (( i=0; i < ($RANDOM%3)+3; i++ )); do
-        pass+=$(get4)
+getPChars() {
+    for (( i=0; i<($RANDOM%10)+4; i++ )); do
+        echo "$1" | sed "s/./&\n/g" | grep . | shuf | head -1
     done
-    echo "${pass}"
 }
 
-for user in users; do
-    echo "$(dopass)" | sudo passwd -s "${user}"
-done
-printf "Change root password:\n"
-sudo passwd root
+dopass2() {
+    chars=''
+    for s in lowerc upperc numc special; do
+        echo $(getPChars "${!s}") | shuf
+    done | tr -d " \n"
+    #shuf -e $chars | tr -d " \n"
 
-printf "Change your password:\n"
-passwd $(id -un)
+}
+
+#for user in users; do
+    #echo "$(dopass)" | sudo passwd -s "${user}"
+#done
+#printf "Change root password:\n"
+#sudo passwd root
+
+#printf "Change your password:\n"
+#passwd $($USER)
+for s in lowerc upperc numc special; do
+    echo $(getPChars "${!s}") | shuf
+done | tr -d " "

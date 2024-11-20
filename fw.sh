7b8260c8b896afdb90fa0d840b81fbdc484f4f7a
ldb1987 <ldb1987@outlook.com>
2024-11-20 09:32:14 -0500

    updated fw.sh

--- fw.sh
+++ fw.sh
@@ -1,45 +1,61 @@
 #!/bin/bash
 
 start() {
+    #clear existing rules
     sudo iptables -F
     sudo iptables -t nat -F
     sudo iptables -t mangle -F
+
+    #inbound
+    sudo iptables -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT #accept inbound traffic from established connections
+    sudo iptables -A INPUT -p tcp --sport 5002 -m state --state NEW #datadog rule that heopfully works (it probably won't tho)
+    sudo iptables -A OUTPUT -j LOG --log-prefix "FW_IN: " --log-level info #logging for bloked connections in
+    sudo iptables -A INPUT DROP #deny by default
+
+    #outbound
+    sudo iptables -A OUTPUT -m state --state RELATED,ESTABLISHED -j ACCEPT #allow outbound traffic from established connections
+    sudo iptables -A OUTPUT -p tcp --dport 443 -m state --state NEW -j ACCEPT #allow 443 (for https) traffic out; needed for package managers
+    sudo iptables -A OUTPUT --dport 53 -m state --state NEW -j ACCEPT #allow 53 out; needed for DNS
+    sudo iptables -A OTUPUT -p tcp --dport 5002 -m state --state NEW -j ACCEPT #allow 5002 out; default port for data dog which hopefully prevents firewall from blocking datadog (if the default port is used)
+    sudo iptables -A OUTPUT -j LOG --log-prefix "FW_OUT: " --log-level info #Logging for blocked connections out
+    sudo iptables -A OUTPUT -j DROP #deny by default
+}
+
+universal() {
+    
 }
 
 propaganda() {
-    sudo iptables -A INPUT -p tcp -s 0/0 --dport 80 -j ACCEPT #http
-    sudo iptables -A INPUT -p tcp -s 0/0 --dport 443 -j ACCEPT #https
-    sudo iptables -A INPUT -p tcp -s 0/0 --dport 22 -j ACCEPT #ssh
-    sudo iptables -A INPUT -s 0/0 -j LOG --log-prefix "FW: " --log-level info
-    sudo iptables -A INPUT -s 0/0 -j DROP
+    #inbound
+    sudo iptables -I INPUT 3 -p tcp --dport 80 -m state --state NEW -j ACCEPT #alloy tcp port 80 in for http
+    sudo iptables -A INPUT 4 -p tcp --dport 443 -m state --state NEW -j ACCEPT #https
+    sudo iptables -A INPUT 5 -p tcp --dport 22 -m state --state NEW -j ACCEPT #ssh
+    
+
+    #outbound
+    
 }
 
 wiretap() {
-    sudo iptables -A INPUT -p tcp -s 0/0 --dport 143 -j ACCEPT #imap
-    sudo iptables -A INPUT -p tcp -s 0/0 --dport 993 -j ACCEPT #imap secure
-
-    sudo iptables -A INPUT -p tcp -s 0/0 --dport 25 -j ACCEPT #smtp
-    sudo iptables -A INPUT -p tcp -s 0/0 --dport 2525 -j ACCEPT
-    sudo iptables -A INPUT -p tcp -s 0/0 --dport 465 -j ACCEPT
-    sudo iptables -A INPUT -p tcp -s 0/0 --dport 587 -j ACCEPT #smtp secure
-    
-    
-    sudo iptables -A INPUT -s 0/0 -j LOG --log-prefix "FW: " --log-level info
-    sudo iptables -A INPUT -s 0/0 -j DROP
+    sudo iptables -A INPUT 5 -p tcp --dport 143 -m state --state NEW -j ACCEPT #imap insecure
+    sudo iptables -A INPUT 5 -p tcp --dport 993 -m state --state NEW -j ACCEPT #imap secure
+    sudo iptables -A INPUT 5 -p tcp --dport 25 -m state --state NEW -j ACCEPT #smtp insecure
+    sudo iptables -A INPUT 5 -p tcp --dport 2525 -m state --state NEW -j ACCEPT #smtp insecure
+    sudo iptables -A INPUT 5 -p tcp --dport 465 -m state --state NEW -j ACCEPT #smtp insecure
+    sudo iptables -A INPUT 5 -p tcp --dport 587 -m state --state NEW -j ACCEPT #smtp secure
 }
 
 vault() {
-    sudo iptables -A INPUT -p tcp -s 0/0 --dport 20 -j ACCEPT #ftp control
-    sudo iptables -A INPUT -p tcp -s 0/0 --dport 21 -j ACCEPT #ftp file
-    sudo iptables -A INPUT -s 0/0 -j LOG --log-prefix "FW: " --log-level info
-    sudo iptables -A INPUT -s 0/0 -j DROP
+    sudo iptables -A INPUT 5 -p tcp --dport 20 -m state --state NEW -j ACCEPT
+    sudo iptables -A INPUT 5 -p tcp --dport 21 -m state --state NEW -j ACCEPT
 }
 
 addon() {
     declare -a ports
     IFS="," read -r -a ports <<< "$1"
+
     for port in "${ports[@]}"; do
-        sudo iptables -I INPUT 0 -s 0/0 --dport "$port" -j ACCEPT
+        sudo iptables -A INPUT 5 -p tcp "--'$2'port" "$port" -m state --state NEW -j ACCEPT
     done
 }
 
@@ -60,6 +76,13 @@ while getopts "hpwva:" option; do
             vault
             exit;;
         a)
-            addon $OPTARG;;
+            ports=$OPTARG;;
+        d)
+            addon "$ports" d
+            exit;;
+        s)
+            addon "$ports" s
+            exit;;
+        
     esac
 done

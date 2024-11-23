2ef2f842cbf6881a8cbfa301eb2b7fbc6ffdfdf8
ldb1987 <ldb1987@outlook.com>
2024-11-23 12:54:15 -0500

    changed to use chpasswd

--- pass2.sh
+++ pass2.sh
@@ -7,7 +7,7 @@ fi
 
 for user in president vicepresident defenseminister secretary general admiral judge bodyguard cabinetofficial treasurer; do
     pass=$(tr -dc 'A-Za-z0-9!@#$=' < /dev/urandom | head -c 14)
-    echo "$pass" | passwd --stdin $user 1>/dev/null
+    echo "$user:$pass" | chpasswd
     if [[ "$1" == "-s" ]]; then
         echo "$user : $pass"
     fi
